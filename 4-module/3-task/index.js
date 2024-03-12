82db50e4a888516a75a85ddf15cbeaaaaff1b18e
Anna Nosova <50626106+AnnaNosova@users.noreply.github.com>
2024-03-12 22:38:18 +0300

    hw 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+  let teachersList = document.querySelectorAll('tbody tr');
+  
+  for (let teacher of teachersList) {
+    const isAvailable = teacher.cells[3].dataset.available;
+
+    if (isAvailable === 'true') {
+      teacher.classList.add('available');
+    } else if (isAvailable === 'false') {
+      teacher.classList.add('unavailable');
+    } else if (!isAvailable) {
+      teacher.hidden = true;
+    }
+
+    const gender = teacher.cells[2].textContent;
+
+    if (gender === 'f') {
+      teacher.classList.add('female');
+    } else if (gender === 'm') {
+      teacher.classList.add('male');
+    }
+
+    const age = Number(teacher.cells[1].textContent);
+    
+    if (age < 18) {
+      teacher.style.textDecoration = 'line-through';
+    }
+  }
 }
