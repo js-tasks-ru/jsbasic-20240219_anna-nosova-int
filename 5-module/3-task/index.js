bf39aa32aaff40f2f7aac72aa2e86f2482b5952f
Anna Nosova <50626106+AnnaNosova@users.noreply.github.com>
2024-03-13 22:56:01 +0300

    hw 4 & 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel');
+  const carouselItems = carousel.querySelector('.carousel__inner');
+  const arrowLeft = carousel.querySelector('.carousel__arrow_left');
+  const arrowRight = carousel.querySelector('.carousel__arrow_right');
+  const itemWidth = carouselItems.querySelector('.carousel__slide').offsetWidth;
+  let slidesQty = carouselItems.querySelectorAll('.carousel__slide').length;
+  let counter = 1;
+
+  arrowLeft.style.display = 'none';
+
+  function checkArrows() {
+    if (counter >= slidesQty) {
+      arrowRight.style.display = 'none';
+      return;
+    } else {
+      arrowRight.style.display = '';
+    }
+    if (counter > 1) {
+      arrowLeft.style.display = '';
+      return;
+    } else {
+      arrowLeft.style.display = 'none';
+    }
+  }
+
+  arrowRight.addEventListener('click', event => {
+    carouselItems.style.transform = `translateX(${-itemWidth * counter}px)`;
+    counter++;
+    checkArrows();
+  });
+
+  arrowLeft.addEventListener('click', event => {
+    --counter;
+    carouselItems.style.transform = `translateX(${-itemWidth * (counter - 1)}px)`;
+    checkArrows();
+  });
 }
