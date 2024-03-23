9a15d5785ccf25aa69fdd3c4d8b1677fc1c7d7e7
Anna Nosova <50626106+AnnaNosova@users.noreply.github.com>
2024-03-23 17:36:08 +0300

    hw 7.1

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,48 +1,38 @@
 import createElement from '../../assets/lib/create-element.js';
+import escapeHtml from '../../assets/lib/escape-html.js';
 
 export default class ProductCard {
-  #elem;
 
   constructor(product) {
     this.product = product;
-    this.#elem = this.createElem();
-    
-    this.addToCartEventListener();
+    this.render();
+    this.addEventListeners();
   }
 
-  get elem() {
-    this._container = this.#elem;
-    return this._container;
-  }
-
-  createElem() {
-    const productCard = createElement(`
+  render() {
+    this.elem = createElement(`
     <div class="card">
       <div class="card__top">
         <img src="/assets/images/products/${this.product.image}" class="card__image" alt="product">
         <span class="card__price">€${this.product.price.toFixed(2)}</span>
       </div>
       <div class="card__body">
-        <div class="card__title">${this.product.name}</div>
+        <div class="card__title">${escapeHtml(this.product.name)}</div>
         <button type="button" class="card__button">
             <img src="/assets/images/icons/plus-icon.svg" alt="icon">
         </button>
       </div>
     </div>`);
-      
-    return productCard;
   }
 
-  addToCartEventListener() {
-    this.#elem.addEventListener('click', event => {
-      const cartBtn = event.target.closest('.card__button');
-      if (cartBtn) {
-        const cartEvent = new CustomEvent('product-add', {
-          detail: this.product.id,
-          bubbles: true,
-        });
-        this.elem.dispatchEvent(cartEvent);
-      }
-    });
+  addEventListeners() {
+    this.elem.onclick = (event) => this.onClick(event);
+  }
+
+  onClick(event) {
+    this.elem.dispatchEvent(new CustomEvent('product-add', {
+      detail: this.product.id,
+      bubbles: true
+    }));
   }
 }
--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,107 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.offsetSize = 350;
+
+    this.render();
+    this.addEventListeners();
+    this.activeCategory = '';
+  }
+
+  sub(ref) {
+    return this.elem.querySelector(`.ribbon__${ref}`);
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="ribbon">
+        <button class="ribbon__arrow ribbon__arrow_left">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon" />
+        </button>
+        <nav class="ribbon__inner"></nav>
+        <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon" />
+        </button>
+      </div>
+    `);
+
+    for (let category of this.categories) {
+      let el = createElement(`<a href="#" class="ribbon__item"></a>`);
+      el.dataset.id = category.id;
+      el.textContent = category.name;
+      
+      this.sub('inner').append(el);
+    }
+
+    this.sub('item').classList.add('ribbon__item_active');
+  }
+
+  scrollLeft() {
+    return this.sub('inner').scrollLeft;
+  }
+
+  scrollRight() {
+    return this.sub('inner').scrollWidth - this.sub('inner').scrollLeft - this.sub('inner').clientWidth;
+  }
+
+  updateArrows() {
+    if (this.scrollLeft() > 0) {
+      this.sub('arrow_left').classList.add('ribbon__arrow_visible');
+    } else {
+      this.sub('arrow_left').classList.remove('ribbon__arrow_visible');
+    }
+
+    if (this.scrollRight() < 1) {
+      this.sub('arrow_right').classList.remove('ribbon__arrow_visible');
+    } else {
+      this.sub('arrow_right').classList.add('ribbon__arrow_visible');
+    }
+  }
+
+  addEventListeners() {
+    this.sub('arrow_left').onclick = (event) => this.onArrowLeftClick(event);
+    this.sub('arrow_right').onclick = (event) => this.onArrowRightClick(event);
+
+    this.elem.onclick = (event) => {
+      let category = event.target.closest('.ribbon__item');
+      if (category) {
+        this.onItemClick(category);
+        event.preventDefault();
+      }
+    };
+
+    this.sub('inner').onscroll = (event) => this.onScroll(event);
+  }
+
+  onArrowRightClick(event) {
+    this.sub('inner').scrollBy(this.offsetSize, 0);
+    this.updateArrows();
+  }
+
+  onArrowLeftClick(event) {
+    this.sub('inner').scrollBy(-this.offsetSize, 0);
+    this.updateArrows();
+  }
+
+  onScroll(event) {
+    this.updateArrows();
+  }
+
+  onItemClick(category) {
+    let isActive = this.sub('item_active');
+    if (isActive) {
+      isActive.classList.remove('ribbon__item_active');
+    }
+
+    category.classList.add('ribbon__item_active');
+
+    this.activeCategory = category.dataset.id;
+
+    this.elem.dispatchEvent(
+      new CustomEvent('ribbon-select', {
+        detail: this.activeCategory,
+        bubbles: true,
+      })
+    );
   }
 }
